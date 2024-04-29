503e24135c2356c9b655f4446f5b11310af6ea7c
ComEngWarda <muawwaznc@gmail.com>
2024-04-28 18:34:19 -0800

    SellerLayout

--- EcommerceDBProject/NewF/EcommerceDbprojectContext.cs
+++ EcommerceDBProject/NewF/EcommerceDbprojectContext.cs
@@ -45,7 +45,7 @@ public partial class EcommerceDbprojectContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-JFOFFG3\\SQLEXPRESS;Initial Catalog=ECommerceDBProject;Trusted_Connection=True;TrustServerCertificate=True");
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-4DBGA3F\\SQLEXPRESS;Initial Catalog=ECommerceDBProject;Trusted_Connection=True;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
