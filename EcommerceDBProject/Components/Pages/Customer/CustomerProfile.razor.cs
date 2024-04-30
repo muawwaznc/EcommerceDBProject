fb3d28c774f6665656c1eb3f34b02eec744219f6
ComEngWarda <muawwaznc@gmail.com>
2024-04-29 18:50:34 -0800

    Customer Profile

---
+++ EcommerceDBProject/Components/Pages/Customer/CustomerProfile.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace EcommerceDBProject.Components.Pages.Customer
+{
+    public partial class CustomerProfile
+    {
+        [Parameter] public string UserDetailId { get; set; }
+        public string UserName { get; set; }
+    }
+}
