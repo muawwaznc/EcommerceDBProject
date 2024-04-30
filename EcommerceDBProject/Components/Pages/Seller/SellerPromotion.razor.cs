31e41bd2091562fd1c433d576100c43b8718e0ad
ComEngWarda <muawwaznc@gmail.com>
2024-04-30 08:34:05 -0800

    SellerPromotion

---
+++ EcommerceDBProject/Components/Pages/Seller/SellerPromotion.razor.cs
@@ -0,0 +1,23 @@
+﻿using EcommerceDBProject.DBContext;
+using EcommerceDBProject.Services.Interface;
+using EcommerceDBProject.Services.Service;
+using Microsoft.AspNetCore.Components;
+
+namespace EcommerceDBProject.Components.Pages.Seller
+{
+    public partial class SellerPromotion
+    {
+        [Inject] IPromotionInterface PromotionService { get; set; }
+        List<Promotion> PromotionList = new List<Promotion>();
+        #region Load Initials
+
+        protected override void OnInitialized()
+        {
+            PromotionList = PromotionService.GetAllProductPromotion();
+        }
+
+
+        #endregion
+
+    }
+}
