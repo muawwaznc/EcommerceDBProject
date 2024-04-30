b32c87d69fc76393be02b502d9ecf30446072922
ComEngWarda <muawwaznc@gmail.com>
2024-04-29 19:52:23 -0800

    SellerProfile Page

---
+++ EcommerceDBProject/Components/Pages/Seller/SellerProfile.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace EcommerceDBProject.Components.Pages.Seller
+{
+    public partial class SellerProfile : ComponentBase
+    {
+        [Parameter] public string UserDetailId { get; set; }
+        public string UserName { get; set; }
+    }
+}
