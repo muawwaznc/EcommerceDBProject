40e2d51d479ce76d23fb8aba2950a7c6137dc1ad
ComEngWarda <muawwaznc@gmail.com>
2024-05-03 00:23:56 -0800

    UpdateDeletePromotion

---
+++ EcommerceDBProject/Components/Pages/Admin/UpdateDeletePromotion.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace EcommerceDBProject.Components.Pages.Admin
+{
+    public partial class UpdateDeletePromotion : ComponentBase
+    {
+
+    }
+}
---
+++ EcommerceDBProject/ViewModels/InitialPageDataForUpdateDeletePromotion.cs
@@ -0,0 +1,12 @@
+﻿using EcommerceDBProject.DBContext;
+
+namespace EcommerceDBProject.ViewModels
+{
+    public class InitialPageDataForUpdateDeletePromotion
+    {
+        public List<Promotion> PromotionsList { get; set; } = new();
+        public Promotion SelectedPromotion { get; set; } = new();
+        public string SelectedPromotionId { get; set; }
+        public bool IsEditDialogBoxOpen { get; set; } = false;
+    }
+}
